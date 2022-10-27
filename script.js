ebec747f1f04fd4ddd78921703809be4af088b0d
Wojciech Górka <wojowany@gmail.com>
2022-10-27 21:23:26 +0200

    BEM input

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let name;
 
-
-document.getElementById("submit-button").onclick = function (){
-    name = document.getElementById("name-input").value;
-    console.log("hello", name);
-}
+document.getElementById("submit-button").onclick = function () {
+  name = document.getElementById("name-input").value;
+  console.log("hello", name);
+};
